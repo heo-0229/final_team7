4037065b68eac31ea5fc89178aa12a6d3a50786f
heo_minkyu <79818840+heo-mk@users.noreply.github.com>
2021-04-27 21:15:14 +0900

    App.js 수정

--- final/src/shared/App.js
+++ final/src/shared/App.js
@@ -24,12 +24,12 @@ function App() {
   const is_cookie = token ? true : false; // 그리고 is_cookie로 토큰 유무판단
   const user_info = useSelector((state) => state.user.user);
 
-  React.useEffect(() => {
-    if (is_cookie) {
-      console.log("로그인 체크");
-      dispatch(userActions.loginCheckAPI(token));
-    } //렌더링 마다 로그인체크
-  }, []);
+  // React.useEffect(() => {
+  //   if (is_cookie) {
+  //     console.log("로그인 체크");
+  //     dispatch(userActions.loginCheckAPI(token));
+  //   } //렌더링 마다 로그인체크
+  // }, []);
 
   return (
     <React.Fragment>
